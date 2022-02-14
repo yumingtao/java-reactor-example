f23291bbe752506a89c23c07580368483aad224f
ymingta <mingtao.yu@daimler.com>
2022-02-14 17:49:39 +0800

    Add flux dynamic method example.

---
+++ src/main/java/com/ymt/example/FluxDynamicExample.java
@@ -0,0 +1,42 @@
+package com.ymt.example;
+
+import reactor.core.publisher.Flux;
+
+/**
+ *
+ * @author yumingtao
+ * @date 2022/2/14 17:26
+ */
+public class FluxDynamicExample {
+    public static void main(String[] args) {
+        FluxDynamicExample fluxDynamicExample = new FluxDynamicExample();
+        fluxDynamicExample.fluxGenerate();
+        fluxDynamicExample.fluxCreate();
+    }
+
+    public void fluxGenerate() {
+        System.out.println("---fluxGenerate---");
+        Flux.generate(sink -> {
+            sink.next("yumingtao");
+            sink.complete();
+        }).subscribe(System.out::println);
+
+        Flux.generate(() -> 1, (i, sink) -> {
+            sink.next(i);
+            if (i == 5) {
+                sink.complete();
+            }
+            return ++i;
+        }).subscribe(System.out::println);
+    }
+
+    public void fluxCreate() {
+        System.out.println("---fluxCreate---");
+        Flux.create(sink -> {
+            for (int i = 0; i < 5; i++) {
+                sink.next(i);
+            }
+            sink.complete();
+        }).subscribe(System.out::println);
+    }
+}
